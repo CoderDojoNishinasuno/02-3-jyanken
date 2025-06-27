263ea708284f0fb644fea7cb5bc2d7bd44944b3c
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-06-27 19:24:13 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,32 @@
+let グーチョキパー = 0
+input.onGesture(Gesture.Shake, function () {
+    グーチョキパー = randint(1, 3)
+    if (グーチョキパー == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (グーチョキパー == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
